89a3c4f3a714e1834387b6c593cf2caef92e260f
saseungmin <dbd02169@naver.com>
2023-03-02 22:17:43 +0900

    test: 공통 버튼 컴포넌트 테스트를 추가하라
    - 버튼 테스트 추가
    - 테스트에 필요한 스타일 theme provider 컴포넌트 추가

---
+++ ui/common/Button.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import MockTheme from 'lib/test/MockTheme';
+
+import Button from './Button';
+
+describe('Button', () => {
+  const renderButton = () => render((
+    <MockTheme>
+      <Button href={given.url}>
+        버튼
+      </Button>
+    </MockTheme>
+  ));
+
+  describe('"href" 속성 유무에 따라 버튼 또는 링크가 나타난다', () => {
+    context('버튼인 경우', () => {
+      given('url', () => undefined);
+
+      it('"href" 속성이 없어야만 한다', () => {
+        renderButton();
+
+        expect(screen.getByText('버튼')).not.toHaveAttribute('href', '/test');
+      });
+    });
+
+    context('링크인 경우', () => {
+      given('url', () => '/test');
+
+      it('href 속성이 존재해야만 한다', () => {
+        renderButton();
+
+        expect(screen.getByText('버튼')).toHaveAttribute('href', '/test');
+      });
+    });
+  });
+});
