3e47cc46a5a18488517defcbd366908babc577d2
harang1 <harang@bemyfriends.com>
2023-03-21 22:49:39 +0900

    feat(useGeolocation): 현재위치를 가져오는 useGeolocation hook을 구현하라
    - 현재위치 정보를 가져오는 useGeolocation custom hook 구현

---
+++ hooks/useGeolocation.ts
@@ -0,0 +1,60 @@
+import { useCallback, useState } from 'react';
+
+type GeoLocation = {
+  loading: boolean;
+  coordinates?: {
+    lat: number | undefined;
+    lng: number | undefined;
+  };
+  error?: {
+    code: number;
+    message: string;
+  }
+};
+
+const useGeoLocation = (): [GeoLocation, () => void] => {
+  const [location, setLocation] = useState<GeoLocation>({
+    loading: false,
+    coordinates: { lat: undefined, lng: undefined },
+  });
+
+  const onSuccess = (position: GeolocationPosition) => {
+    setLocation({
+      loading: false,
+      coordinates: {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      },
+    });
+  };
+
+  const onError = (error: GeolocationPositionError) => {
+    setLocation({
+      loading: false,
+      error: {
+        code: error.code,
+        message: error.message,
+      },
+    });
+  };
+
+  const onGetGeoLocation = useCallback(() => {
+    if (navigator?.geolocation) {
+      setLocation((prev) => ({
+        ...prev,
+        loading: true,
+      }));
+      navigator.geolocation.getCurrentPosition(onSuccess, onError);
+      return;
+    }
+
+    onError({
+      code: 0,
+      message: 'Geolocation not supported',
+    } as GeolocationPositionError);
+  }, []);
+
+  return [location, onGetGeoLocation];
+};
+
+export default useGeoLocation;
