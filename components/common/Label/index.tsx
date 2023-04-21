09dcf799b94ecf039effd34c6fcc7176966918dc
Harang <dbd02169@naver.com>
2023-04-22 04:36:54 +0900

    feat(Label): label 컴포넌트 디자인 시스템을 적용하라 (#96)
    - label 공통 컴포넌트 구현
    - offbeat.place/label

---
+++ components/common/Label/index.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { PropsWithChildren, ReactNode } from 'react';
+
+import clsx from 'clsx';
+import useActionKeyEvent from 'hooks/useActionKeyEvent';
+
+import styles from './index.module.scss';
+
+type Color = 'highlight' | 'done' | 'positive' | 'danger' | 'active' | 'attention' | 'relate';
+
+type Props = {
+  onClick?: () => void;
+  prefixIcon?: ReactNode;
+  suffixIcon?: ReactNode;
+  type?: 'default' | 'reverse';
+  color: Color;
+  size?: 'small' | 'medium';
+};
+
+function Label({
+  onClick, prefixIcon, suffixIcon, color, type = 'default', size = 'medium', children,
+}: PropsWithChildren<Props>) {
+  const onKeyDown = useActionKeyEvent<HTMLDivElement, string[]>(['Enter', 'NumpadEnter'], () => {
+    onClick?.();
+  });
+
+  return (
+    <div
+      role={onClick ? 'button' : undefined}
+      tabIndex={onClick ? 0 : undefined}
+      className={clsx(styles.labelWrapper, {
+        [styles.button]: !!onClick,
+        [styles[size]]: size,
+        [styles[type]]: type,
+        [styles[`default-${size}`]]: type === 'default',
+        [styles[`default-${color}`]]: type === 'default',
+        [styles[`reverse-${color}`]]: type === 'reverse',
+      })}
+      onClick={onClick}
+      onKeyDown={onKeyDown}
+    >
+      <div>{prefixIcon}</div>
+      <div>
+        {children}
+      </div>
+      <div>{suffixIcon}</div>
+    </div>
+  );
+}
+
+export default Label;
