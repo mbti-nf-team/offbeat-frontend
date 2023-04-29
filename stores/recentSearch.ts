d32673d0e615217160d82c9e1623f212f42d692d
saseungmin <dbd02169@naver.com>
2023-04-29 15:42:27 +0900

    feat: 최근 검색기록 리스트 스타일 변경과 삭제 기능을 구현하라
    - 최근 검색기록 10개까지 노출하도록 변경
    - 삭제 버튼 추가
    - 디자인 변경

--- stores/recentSearch.ts
+++ stores/recentSearch.ts
@@ -1,12 +1,18 @@
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
-interface RecentSearchState {
+type RecentSearchState = {
   recentSearchList: string[];
-  addRecentSearch: (nextKeyword: string) => void;
-}
+};
 
-const useRecentSearchStore = create<RecentSearchState>()(
+type RecentSearchAction = {
+  addRecentSearch: (nextKeyword: string) => void;
+  removeRecentSearch: (removeKeyword: string) => void;
+};
+
+type RecentSearch = RecentSearchState & RecentSearchAction;
+
+const useRecentSearchStore = create<RecentSearch>()(
   persist(
     (set, get) => ({
       recentSearchList: [],
@@ -14,14 +20,18 @@ const useRecentSearchStore = create<RecentSearchState>()(
         const prevRecentSearch = get().recentSearchList;
 
         const slicedPrevRecentSearch = prevRecentSearch
-          .length >= 5 ? prevRecentSearch.slice(0, 5) : prevRecentSearch;
+          .length >= 10 ? prevRecentSearch.slice(0, 10) : prevRecentSearch;
 
-        const hasKeyword = slicedPrevRecentSearch.includes(nextKeyword);
+        const filteredKeyword = slicedPrevRecentSearch.filter((keyword) => keyword !== nextKeyword);
 
-        set({
-          recentSearchList: hasKeyword
-            ? slicedPrevRecentSearch : [nextKeyword, ...slicedPrevRecentSearch],
-        });
+        set({ recentSearchList: [nextKeyword, ...filteredKeyword] });
+      },
+      removeRecentSearch: (removeKeyword) => {
+        const recentSearch = get().recentSearchList;
+
+        const nextRecentSearch = recentSearch.filter((keyword) => keyword !== removeKeyword);
+
+        set({ recentSearchList: nextRecentSearch });
       },
     }),
     {
