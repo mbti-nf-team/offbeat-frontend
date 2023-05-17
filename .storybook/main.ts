e5f1248d3fb9117a464b290b06d4fc79e6014f39
saseungmin <dbd02169@naver.com>
2023-05-17 23:30:57 +0900

    chore(dependency): @svgr/webpack update dependency

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -51,10 +51,17 @@ const config: StorybookConfig = {
         loader: '@svgr/webpack',
         options: {
           svgoConfig: {
-            plugins: [{
-              name: 'removeViewBox',
-              active: false,
-            }],
+            plugins: [
+              {
+                name: 'preset-default',
+                params: {
+                  overrides: {
+                    removeViewBox: false,
+                    active: false,
+                  },
+                },
+              },
+            ],
           },
         },
       }],
