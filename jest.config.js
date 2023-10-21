676a4fc3021ab360b7e4cbaaf9fe2b871e7268e5
Harang <dbd02169@naver.com>
2023-10-21 21:13:19 +0900

    fix: jest에서 svg 테스트시 에러나는 이슈 수정 (#207)

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,7 @@ const customJestConfig = {
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
-    '^.+\\.(svg)$': '<rootDir>/__mocks__/svg.js',
+    '^.+\\.(svg)$': '<rootDir>/__mocks__/svg.tsx',
   },
   testPathIgnorePatterns: [
     'node_modules',
