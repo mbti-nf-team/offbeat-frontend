ece61de956d9f34ec490d59ca7fc25d81b805166
Harang <dbd02169@naver.com>
2023-10-23 23:13:25 +0900

    refactor: naver search api와 google textsearch api 통합하여 bff에서 호출하도록 리팩터링 (#211)

---
+++ src/app/api/search/places/route.ts
@@ -0,0 +1,69 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { TextSearchResponseData } from '@googlemaps/google-maps-services-js';
+import { checkEmpty } from '@nf-team/core';
+
+import { paramsSerializer } from '@/lib/apis';
+import { fetchAllSettledSearchNaverBlogs } from '@/lib/apis/search';
+import { filteredPlaces } from '@/utils';
+
+export const runtime = 'nodejs';
+
+const TEN_MINUTES = 600;
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const requestHeaders = new Headers(request.headers);
+
+  const query = searchParams.get('query');
+  const nextCursor = searchParams.get('nextCursor');
+
+  const textSearchResponse = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/google/search?${paramsSerializer({
+    query,
+    nextCursor,
+  })}`, {
+    method: 'GET',
+    next: {
+      revalidate: TEN_MINUTES,
+    },
+    headers: requestHeaders,
+  });
+
+  if (!textSearchResponse.ok) {
+    return NextResponse.json(null, textSearchResponse);
+  }
+
+  const places = await textSearchResponse.json() as TextSearchResponseData;
+
+  const placesResult = filteredPlaces(checkEmpty(places?.results));
+  const placeName = placesResult.filter((place) => !!place?.name).map((place) => place.name);
+
+  const searchBlogPosts = await fetchAllSettledSearchNaverBlogs({ placeName }, {
+    method: 'GET',
+    next: {
+      revalidate: TEN_MINUTES,
+    },
+    headers: {
+      ...requestHeaders,
+      'nfteam-api-token': process.env.API_HEADER_TOKEN,
+    },
+  });
+
+  const response = placesResult.map((place, index) => ({
+    ...place,
+    searchBlogPost: searchBlogPosts[index],
+  }));
+
+  return NextResponse.json({
+    ...places,
+    results: response,
+  }, {
+    ...textSearchResponse,
+    headers: {
+      ...textSearchResponse.headers,
+      'Cache-Control': 'public, s-maxage=1',
+      'CDN-Cache-Control': 'public, s-maxage=60',
+      'Vercel-CDN-Cache-Control': `public, s-maxage=${TEN_MINUTES}`,
+    },
+  });
+}
--- src/lib/apis/search/model.ts
+++ src/lib/apis/search/model.ts
@@ -1,9 +1,11 @@
 import { PlaceDetailsResponseData, TextSearchResponseData } from '@googlemaps/google-maps-services-js';
 
-import { NaverSearchBlog } from '@/lib/types/search';
+import { NaverSearchBlog, SearchPlaces } from '@/lib/types/search';
 
 export type NaverSearchBlogResponse<T> = NaverSearchBlog<T>;
 
 export type TextSearchPlaceResponse = TextSearchResponseData;
 
 export type PlaceDetailResponse = PlaceDetailsResponseData;
+
+export type SearchPlacesResponse = SearchPlaces;
