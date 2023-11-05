4776c5d1674819044a8f72284ad133ab7491d575
saseungmin <dbd02169@naver.com>
2023-11-05 23:02:26 +0900

    chore: new place api 타입 정의

--- @types/environment.d.ts
+++ @types/environment.d.ts
@@ -2,10 +2,9 @@
 namespace NodeJS {
   interface ProcessEnv extends NodeJS.ProcessEnv {
     NEXT_PUBLIC_GOOGLE_MAP_API_KEY: string;
-    NEXT_PUBLIC_NAVER_SEARCH_CLIEND_ID: string;
-    NEXT_PUBLIC_NAVER_SEARCH_CLIEND_SECRET: string;
     NEXT_PUBLIC_ORIGIN: string;
     NEXT_PUBLIC_API_HOST: string;
     API_HEADER_TOKEN: string;
+    NEW_GOOGLE_MAP_API_ORIGIN: string;
   }
 }
