09fdddd1061c0abea376eb06a3bbc9e59f0f39e8
saseungmin <dbd02169@naver.com>
2023-12-04 23:04:17 +0900

    perf: next loca font의 prerendard 폰트 subset으로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,59 +1,16 @@
 import { ReactNode } from 'react';
 
-import localFont from 'next/font/local';
 import Script from 'next/script';
 
 import Layout from '@/components/common/Layout';
 import Toast from '@/components/common/Toast';
 
+import pretendardFont from './fonts';
 import Providers from './providers';
 
 import 'src/styles/normalize.css';
 import 'src/styles/bottomSheet.css';
 import 'src/styles/global.scss';
-
-const pretendardFont = localFont({
-  src: [
-    {
-      path: './fonts/Pretendard-Bold.woff2',
-      weight: 'bold',
-      style: 'normal',
-    },
-    {
-      path: './fonts/Pretendard-SemiBold.woff2',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: './fonts/Pretendard-Medium.woff2',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: './fonts/Pretendard-Regular.woff2',
-      weight: 'normal',
-      style: 'normal',
-    },
-  ],
-  display: 'swap',
-  fallback: [
-    'Pretendard Variable',
-    'Pretendard',
-    '-apple-system',
-    'BlinkMacSystemFont',
-    'system-ui',
-    'Roboto',
-    'Helvetica Neue',
-    'Segoe UI',
-    'Apple SD Gothic Neo',
-    'Noto Sans KR',
-    'Malgun Gothic',
-    'Apple Color Emoji',
-    'Segoe UI Emoji',
-    'Segoe UI Symbol',
-    'sans-serif',
-  ],
-});
 
 function RootLayout({ children }: { children: ReactNode }) {
   return (
