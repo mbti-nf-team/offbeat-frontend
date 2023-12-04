429a9d33f20eba881dc5fa2d6cbfc157e1a93bc5
Harang <dbd02169@naver.com>
2023-12-04 23:10:16 +0900

    perf: next local font의 prerendard 폰트 subset으로 변경 (#249)

---
+++ src/app/fonts/index.ts
@@ -0,0 +1,47 @@
+import localFont from 'next/font/local';
+
+const pretendardFont = localFont({
+  src: [
+    {
+      path: './Pretendard-Bold.subset.woff2',
+      weight: 'bold',
+      style: 'normal',
+    },
+    {
+      path: './Pretendard-SemiBold.subset.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './Pretendard-Medium.subset.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './Pretendard-Regular.subset.woff2',
+      weight: 'normal',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  preload: true,
+  fallback: [
+    'Pretendard Variable',
+    'Pretendard',
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'system-ui',
+    'Roboto',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Apple SD Gothic Neo',
+    'Noto Sans KR',
+    'Malgun Gothic',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'sans-serif',
+  ],
+});
+
+export default pretendardFont;
