fa0b121f50a54a0dfe7ebdff262514388f262df8
saseungmin <dbd02169@naver.com>
2024-03-14 01:20:57 +0900

    chore: place detail 페이지 metadata 적용

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -1,89 +1,24 @@
-import { Metadata, ResolvingMetadata } from 'next';
+import { Metadata } from 'next';
 import { cookies } from 'next/headers';
-
-import { Language, Status } from '@googlemaps/google-maps-services-js';
-import { checkEmpty } from '@nf-team/core';
 
 import MapContainer from '@/components/map/MapContainer';
 import MapLayout from '@/components/map/MapLayout';
-import { paramsSerializer } from '@/lib/apis';
 import { getUser } from '@/lib/apis/auth';
 import CookieNames from '@/lib/constants/cookies';
 
-import { getGooglePlaceDetails, getPlacePhotoUrl } from '../api/handler';
-import { metadata } from '../page';
-
-const description = '이 장소는 ✌️진짜✌️ 로컬 여행지일까? 확인해보세요!';
+import { metadata as defaultMetadata } from '../page';
 
 type Props = {
   searchParams: { [key: string]: string | undefined; };
 };
 
-export async function generateMetadata(
-  { searchParams }: Props,
-  parent: ResolvingMetadata,
-): Promise<Metadata> {
-  const defaultUrl = `${process.env.NEXT_PUBLIC_ORIGIN}/maps`;
-  const defaultMetadata: Metadata = {
-    ...metadata,
-    openGraph: {
-      ...metadata.openGraph,
-      url: defaultUrl,
-    },
-  };
-
-  if (!searchParams?.id) {
-    return defaultMetadata;
-  }
-
-  try {
-    const placeDetails = await getGooglePlaceDetails({
-      place_id: searchParams.id,
-      language: Language.ko,
-      region: 'KR',
-      fields: ['photos', 'name', 'place_id'],
-    });
-
-    if (placeDetails.status !== Status.OK || !placeDetails?.result) {
-      return defaultMetadata;
-    }
-
-    const thumbnailPhotoUrl = getPlacePhotoUrl(placeDetails.result.photos?.[0].photo_reference);
-
-    const previousParent = await parent;
-    const previousImages = checkEmpty(previousParent.openGraph?.images);
-    const images = thumbnailPhotoUrl ? [
-      {
-        url: thumbnailPhotoUrl,
-        width: 800,
-        height: 600,
-      }, ...previousImages,
-    ] : previousImages;
-    const title = placeDetails.result?.name || metadata.title;
-
-    return {
-      metadataBase: new URL(process.env.NEXT_PUBLIC_ORIGIN),
-      title: placeDetails.result?.name ? `${metadata.title} - ${placeDetails.result?.name}` : metadata.title,
-      description,
-      openGraph: {
-        title,
-        description,
-        images,
-        url: `${defaultUrl}?${paramsSerializer({
-          country: searchParams?.country,
-          id: placeDetails.result.place_id,
-        })}`,
-      },
-      twitter: {
-        title,
-        description,
-        images,
-      },
-    };
-  } catch (error) {
-    return defaultMetadata;
-  }
-}
+export const metadata: Metadata = {
+  ...defaultMetadata,
+  openGraph: {
+    ...defaultMetadata.openGraph,
+    url: `${process.env.NEXT_PUBLIC_ORIGIN}/maps`,
+  },
+};
 
 async function Page({ searchParams }: Props) {
   const cookiesStore = cookies();
