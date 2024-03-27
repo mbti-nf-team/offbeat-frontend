4b32da79c390da17389d398995cc0c95a3b230c9
saseungmin <dbd02169@naver.com>
2024-03-27 23:31:31 +0900

    chore: 로그인되어있지 않으면 좋아요 목록 접근 불가능하도록 설정

--- src/app/my/favorite-places/page.tsx
+++ src/app/my/favorite-places/page.tsx
@@ -1,6 +1,17 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
 import FavoritePlacesPage from '@/components/my/FavoritePlacesPage';
+import CookieNames from '@/lib/constants/cookies';
 
 function Page() {
+  const cookiesStore = cookies();
+  const accessToken = cookiesStore.get(CookieNames.ACCESS_TOKEN);
+
+  if (!accessToken?.value) {
+    redirect('/');
+  }
+
   return (
     <FavoritePlacesPage />
   );
