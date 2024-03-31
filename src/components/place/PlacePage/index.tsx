11d1bce7ce67da926c41fba84f811692089894d6
saseungmin <dbd02169@naver.com>
2024-03-31 23:52:36 +0900

    fix: build error

--- src/components/place/PlacePage/index.tsx
+++ src/components/place/PlacePage/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Route } from 'next';
 import { useRouter } from 'next/navigation';
 
 import { useIsomorphicLayoutEffect } from '@nf-team/react';
@@ -14,7 +15,7 @@ function PlacePage({ id }: Props) {
   const router = useRouter();
 
   const onCloseDetailWindow = () => {
-    router.push('/');
+    router.push('/' as Route);
   };
 
   useIsomorphicLayoutEffect(() => {
